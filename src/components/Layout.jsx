b5521da0d89a30a5b02a83280b417e3bd8876014
martinko-nb995-btw <admin@0z6sw.onmicrosoft.com>
2026-08-29 12:41:45 +0200

    feat: update documentation with SMTP settings, add TODO list, and enhance LDAP integration details

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -212,7 +212,7 @@ export function Layout({ children, title, tableOfContents }) {
           </main>
           <Footer />
         </div>
-        {!router.route.startsWith("/mgmtctl") && <div
+        {!router.route.startsWith("/nothing") && <div
             className="hidden xl:sticky xl:top-[4.5rem] xl:-mr-6 xl:block xl:h-[calc(100vh-4.5rem)] xl:flex-none xl:overflow-y-auto xl:py-16 xl:pr-6 pl-12"
             style={{ top: `calc(${bannerHeight}px + 4.5rem)` }}
         >
